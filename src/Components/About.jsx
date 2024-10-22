b12d37466683909640d4b6151d963891fa5e7a5c
charansingh14853 <156320751+charansingh14853@users.noreply.github.com>
2024-10-22 13:03:08 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,47 +5,35 @@
  */
 
 import React from "react";
-
-/**
- * About background image
- *
- * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
- * represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a image you
- * freely use on your site.
- */
 import image from "../images/motion-background.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
 /**
- * Sort description that expands on your title on the Home component.
+ * Short description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a passionate B.Tech graduate from Presidency University, Bengaluru, specializing in Artificial Intelligence and Machine Learning. I love working on impactful AI/ML projects that drive innovation and solve real-world challenges.";
 
 /**
- * List of some of skills or technologies you work on, are learning,
- * passionate about, or enjoy,
+ * List of some skills or technologies you work on, are learning,
+ * passionate about, or enjoy.
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Python",
+  "HTML & CSS",
+  "MySQL",
+  "Data Analysis",
+  "Robotic Process Automation (RPA)",
+  "Web Design",
+  "Java script",
 ];
 
 /**
- * Use this to give more information about what you are passionate about,
- * how you best work, or even a quote. This will help someone learn more
- * about you on a professional level.
+ * Additional details about your passion, work style, or a professional quote.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I believe in leveraging AI/ML technologies to solve complex problems and contribute to projects that make a meaningful impact. I continually strive for innovation and collaboration in a dynamic tech environment.";
 
 const About = () => {
   return (
